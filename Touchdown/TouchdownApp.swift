28431eb87cfd278877a107470bca81b8eee5a227
Abdelaziz Bennacer <a.bennacer@compethance.fr>
2024-05-10 09:06:50 +0200

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Abdelaziz Bennacer on 10/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
